1a4561652a11063cbec791e6dacab4ffe0b09a28
marisolcastrolopez <solcastrodg@gmail.com>
2019-06-28 12:36:29 -0500

    Ant design library. Popup menu used

--- src/App.js
+++ src/App.js
@@ -1,103 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Sidenav, Toggle, Nav, Icon, Dropdown, Navbar } from 'rsuite';
-import 'rsuite/dist/styles/rsuite.min.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      expanded: true,
-      activeKey: '1'
-    };
-    this.handleToggle = this.handleToggle.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  handleToggle() {
-    this.setState({
-      expanded: !this.state.expanded
-    });
-  }
-  handleSelect(eventKey) {
-    this.setState({
-      activeKey: eventKey
-    });
-  }
-  render() {
-    const { expanded } = this.state;
-
-    return (
-      <div>
-       <Navbar>
-    <Navbar.Header>
-      <a href="#" className="navbar-brand logo">RSUITE</a>
-    </Navbar.Header>
-    <Navbar.Body>
-      <Nav>
-        <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>
-        <Nav.Item>News</Nav.Item>
-        <Nav.Item>Products</Nav.Item>
-        <Dropdown title="About">
-          <Dropdown.Item>Company</Dropdown.Item>
-          <Dropdown.Item>Team</Dropdown.Item>
-          <Dropdown.Item>Contact</Dropdown.Item>
-        </Dropdown>
-      </Nav>
-      <Nav pullRight>
-        <Nav.Item icon={<Icon icon="cog" />} >Settings</Nav.Item>
-      </Nav>
-    </Navbar.Body>
-  </Navbar>
-      <div style={{ width: 250 }}>
-        <Toggle onChange={this.handleToggle} checked={expanded} />
-        <hr />
-        <Sidenav
-          expanded={expanded}
-          defaultOpenKeys={['3', '4']}
-          activeKey={this.state.activeKey}
-          onSelect={this.handleSelect}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <Sidenav.Body>
-            <Nav>
-              <Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
-                Dashboard
-              </Nav.Item>
-              <Nav.Item eventKey="2" icon={<Icon icon="group" />}>
-                User Group
-              </Nav.Item>
-              <Dropdown
-                placement="rightTop"
-                eventKey="3"
-                title="Advanced"
-                icon={<Icon icon="magic" />}
-              >
-                <Dropdown.Item eventKey="3-1">Geo</Dropdown.Item>
-                <Dropdown.Item eventKey="3-2">Devices</Dropdown.Item>
-                <Dropdown.Item eventKey="3-3">Loyalty</Dropdown.Item>
-                <Dropdown.Item eventKey="3-4">Visit Depth</Dropdown.Item>
-              </Dropdown>
-              <Dropdown
-                placement="rightTop"
-                eventKey="4"
-                title="Settings"
-                icon={<Icon icon="gear-circle" />}
-              >
-                <Dropdown.Item eventKey="4-1">Applications</Dropdown.Item>
-                <Dropdown.Item eventKey="4-2">Channels</Dropdown.Item>
-                <Dropdown.Item eventKey="4-3">Versions</Dropdown.Item>
-                <Dropdown.Menu eventKey="4-5" title="Custom Action">
-                  <Dropdown.Item eventKey="4-5-1">Action Name</Dropdown.Item>
-                  <Dropdown.Item eventKey="4-5-2">Action Params</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Nav>
-          </Sidenav.Body>
-        </Sidenav>
-      </div>
-      </div>
-    );
-  }
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React from "react";
+import { render } from "react-dom";
+import Popup from "reactjs-popup";
+import Hello from "./Hello";
+import BurgerIcon from "./BurgerIcon";
+import Menu from "./Menu";
+import "./index.css";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  marginTop: "40px"
+};
+const contentStyle = {
+  background: "rgba(255,255,255,0)",
+  width: "80%",
+  border: "none"
+};
+
+const App = () => (
+  <div style={styles}>
+    <Hello name="Create cool burger menu using reactjs-popup" />
+    <Popup
+      modal
+      overlayStyle={{ background: "rgba(255,255,255,0.98" }}
+      contentStyle={contentStyle}
+      closeOnDocumentClick={false}
+      trigger={open => <BurgerIcon open={open} />}
+    >
+      {close => <Menu close={close} />}
+    </Popup>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
+
