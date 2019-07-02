ae9e78f01565057399231167e9480f0e3032b1a8
marisolcastrolopez <solcastrodg@gmail.com>
2019-07-01 19:27:30 -0500

    Updates

---
+++ precache-manifest.1179f1d1b8bf609f9554dc97ae1681ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6199933a628f02fd99a161c90388abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac515c09a0e2451cfb3d",
+    "url": "/static/css/main.ba56ba53.chunk.css"
+  },
+  {
+    "revision": "10e9639078e396b88221",
+    "url": "/static/js/2.12687912.chunk.js"
+  },
+  {
+    "revision": "ac515c09a0e2451cfb3d",
+    "url": "/static/js/main.faee90d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
