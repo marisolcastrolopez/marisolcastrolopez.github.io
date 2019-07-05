738fec4aa03a2c22ae5b229b0d6445637ffb816b
marisolcastrolopez <solcastrodg@gmail.com>
2019-07-04 19:44:35 -0500

    React spring animation only name and description

--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
 import "../../css/index.css";
 import { Transition, animated } from "react-spring/renderprops";
+// import { useSpring } from 'react-spring'
 // import 'antd/dist/antd.css';
+
+// const props = useSpring({
+//   opacity: 1,
+//   from: { opacity: 0 },
+// })
+// return <animated.h1 style={props}>hello</animated.h1>
+// function Demo() {
+//   let elseA = useSpring({
+//       opacity: 0,
+//       from: { opacity: 0 },
+//     })
+//   return (
+//       <animated.h1 style={elseA}>hello</animated.h1>
+//   )
+// }
 
 export default class HomeContent extends Component {
   state = { show: true };
@@ -14,7 +30,6 @@ export default class HomeContent extends Component {
             items={this.state.show}
             from={{ position: "absolute", overflow: "hidden", height: 0 }}
             enter={[{ height: "auto" }]}
-            leave={{ height: 0 }}
           >
             {show =>
               show &&
@@ -24,6 +39,10 @@ export default class HomeContent extends Component {
                     Hello! I'm <br />
                     <span className="boldTitle">Marisol Castro</span>
                   </span>
+                  <br />
+                  <span className="sizeMedium">
+                    I'm a Frontend Developer and Graphic Designer.
+                  </span>
                 </animated.div>
               ))
             }
@@ -32,18 +51,35 @@ export default class HomeContent extends Component {
         <br />
         <br />
         <br />
-
-        {/* <span className="titles">Hello! I'm <br/><span className="boldTitle">Marisol Castro</span></span> */}
-        <br />
-        <span className="sizeMedium">
+        {/* <div className="reveals-main">
+          <Transition
+            native
+            items={this.state.show}
+            from={{ position: "absolute", overflow: "hidden", height: 0 }}
+            enter={[{ height: "auto" }]}
+          >
+            {show =>
+              show &&
+              (props => (
+                <animated.div style={{ opacity: 1, from: { opacity: 0 }}}>
+                  <span className="sizeMedium">
+                    I'm a Frontend Developer and Graphic Designer.
+                  </span>
+                </animated.div>
+              ))
+            }
+          </Transition>
+        </div> */}
+        {/* <span className="sizeMedium">
           I'm a Frontend Developer and Graphic Designer.
-        </span>
+        </span> */}
         <br />
         <br />
         <span>
-          I love details, I like to be meticulous in everthing that I do. I feel
-          passion about science,technology UX and UI, it amuse me.
+          I am a detail-oriented person, I tend to be meticulous in everthing
+          that I do. I feel passion about science,technology UX and UI.
         </span>
+        {/* <Demo/> */}
       </div>
     );
   }
