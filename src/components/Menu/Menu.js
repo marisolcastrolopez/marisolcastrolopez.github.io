e34c350bea1340b83f3873691fa248b072a8bc00
marisolcastrolopez <solcastrodg@gmail.com>
2019-07-10 19:57:02 -0500

    Logo in menu added

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,30 +1,58 @@
 import React from "react";
 import { Icon } from "antd";
 import { Link } from "react-router-dom";
+import logoImgPurple from "../../img/logo-port-m-purple.png";
 
 export default ({ close }) => (
   <div className="menu">
     <ul>
-      <li onClick={close}>
-        <Link className="menu-links" to="/">Home</Link>
+      <li>
+        <img
+          className="logo-img"
+          src={logoImgPurple}
+          style={{
+            width: `${40}%`,
+            maxWidth: `${5}em`,
+            height: "auto",
+            zIndex: 9999
+          }}
+          alt="logo-img-purple"
+        />
       </li>
       <li onClick={close}>
-        <Link className="menu-links" to="/burgerqueen">Burger Queen</Link>
+        <Link className="menu-links" to="/">
+          Home
+        </Link>
       </li>
       <li onClick={close}>
-        <Link className="menu-links" to="/sharesmile">Share a Smile</Link>
+        <Link className="menu-links" to="/burgerqueen">
+          Burger Queen
+        </Link>
       </li>
       <li onClick={close}>
-        <Link className="menu-links" to="/eyeroad">Eye Road</Link>
+        <Link className="menu-links" to="/sharesmile">
+          Share a Smile
+        </Link>
       </li>
       <li onClick={close}>
-        <Link className="menu-links" to="/alyapp">Aly App</Link>
+        <Link className="menu-links" to="/eyeroad">
+          Eye Road
+        </Link>
       </li>
       <li onClick={close}>
-        <Link className="menu-links" to="/aboutme">About Me</Link>
+        <Link className="menu-links" to="/alyapp">
+          Aly App
+        </Link>
       </li>
       <li onClick={close}>
-        <Link className="menu-links" to="/burgerqueen">Let's talk</Link>
+        <Link className="menu-links" to="/aboutme">
+          About Me
+        </Link>
+      </li>
+      <li onClick={close}>
+        <Link className="menu-links" to="/burgerqueen">
+          Let's talk
+        </Link>
       </li>
       <br />
       <li>
