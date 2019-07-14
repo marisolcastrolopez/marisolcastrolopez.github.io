7a186c0b3b239b15b4fe2ceba7495b5018442610
marisolcastrolopez <solcastrodg@gmail.com>
2019-07-13 20:38:59 -0500

    Hash router testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppRouter from "./router";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <AppRouter />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
