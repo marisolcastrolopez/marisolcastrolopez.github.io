8a96771bdbe4619178b47ac9c7ff35be26ac9824
marisolcastrolopez <solcastrodg@gmail.com>
2019-07-31 09:50:48 -0500

    Menu fixed design and responsiviness

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Icon } from "antd";
+import React, { Fragment } from "react";
+import { Icon, Row, Col } from "antd";
 import { Link } from "react-router-dom";
 import logoImgPurple from "../../img/logo-port-m-purple.png";
 import { Spring } from "react-spring/renderprops";
@@ -9,95 +9,140 @@ export default ({ close }) => (
     delay={150}
     from={{ opacity: 0, marginLeft: 0 }}
     to={{ opacity: 1, marginLeft: 0 }}
-    leave={ {opacity: 0} }
+    leave={{ opacity: 0 }}
   >
     {props => (
-      <div style={props} className="menu">
-        <ul>
-          <li>
-            <img
-              className="logo-img"
-              src={logoImgPurple}
-              style={{
-                width: `${40}%`,
-                maxWidth: `${5}em`,
-                height: "auto",
-                zIndex: 9999
-              }}
-              alt="logo-img-purple"
-            />
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/">
-              Home
-            </Link>
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/burgerqueen">
-              Burger Queen
-            </Link>
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/sharesmile">
-              Share a Smile
-            </Link>
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/eyeroad">
-              Eye Road
-            </Link>
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/alyapp">
-              Aly App
-            </Link>
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/aboutme">
-              About Me
-            </Link>
-          </li>
-          <li onClick={close}>
-            <Link className="menu-links" to="/burgerqueen">
-              Let's talk
-            </Link>
-          </li>
-          <br />
-          <li>
-            <a
-              href={"https://www.linkedin.com/in/marisolcastrolopez/"}
-              target={"_blank"}
-            >
-              <Icon
-                type="linkedin"
-                className="padFAwe"
-                style={{ fontSize: "35px", color: "#da32ab" }}
+      <Fragment>
+        <div style={props} className="menu">
+          <ul>
+            <li>
+              <img
+                className="logo-img"
+                src={logoImgPurple}
+                style={{
+                  width: `${40}%`,
+                  maxWidth: `${5}em`,
+                  height: "auto",
+                  zIndex: 9999
+                }}
+                alt="logo-img-purple"
               />
-            </a>
-            <a
-              href={"https://github.com/marisolcastrolopez/"}
-              target={"_blank"}
-            >
-              <Icon
-                type="github"
-                className="padFAwe"
-                style={{ fontSize: "35px", color: "#da32ab" }}
-              />
-            </a>
-            <a
-              href={"https://www.linkedin.com/in/marisolcastrolopez/"}
-              target={"_blank"}
-            >
-              <Icon
-                type="mail"
-                className="padFAwe"
-                style={{ fontSize: "35px", color: "#da32ab" }}
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/">
+                Home
+              </Link>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/burgerqueen">
+                Burger Queen
+              </Link>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/sharesmile">
+                Share a Smile
+              </Link>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/eyeroad">
+                Eye Road
+              </Link>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/alyapp">
+                Aly App
+              </Link>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/aboutme">
+                About Me
+              </Link>
+            </li>
+            <li onClick={close}>
+              <Link className="menu-links" to="/burgerqueen">
+                Let's talk
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Row style={{ textAlign: "center" }} type="flex">
+          <Col
+            xs={8}
+            sm={8}
+            md={8}
+            lg={8}
+            xl={8}
+          >
+              <a
+                href={"https://www.linkedin.com/in/marisolcastrolopez/"}
+                target={"_blank"}
+              >
+                <Icon
+                  type="linkedin"
+                  className="padFAwe"
+                  style={{ fontSize: "30px", color: "#da32ab" }}
+                />
+              </a>
+          </Col>
+          <Col
+            xs={8}
+            sm={8}
+            md={8}
+            lg={8}
+            xl={8}
+          >
+              <a
+                href={"https://github.com/marisolcastrolopez/"}
+                target={"_blank"}
+              >
+                <Icon
+                  type="github"
+                  className="padFAwe"
+                  style={{ fontSize: "30px", color: "#da32ab" }}
+                />
+              </a>
+          </Col>
+          <Col
+            xs={8}
+            sm={8}
+            md={8}
+            lg={8}
+            xl={8}
+          >
+              <a
+                href={"https://www.linkedin.com/in/marisolcastrolopez/"}
+                target={"_blank"}
+              >
+                <Icon
+                  type="twitter"
+                  className="padFAwe"
+                  style={{ fontSize: "30px", color: "#da32ab" }}
+                />
+              </a>
+          </Col>
+        </Row>
+        <Row style={{ textAlign: "center" }} type="flex">
+          <Col
+            xs={24}
+            sm={24}
+            md={24}
+            lg={24}
+            xl={24}
+          >
+              {/* <a
+                href={"https://www.linkedin.com/in/marisolcastrolopez/"}
+                target={"_blank"}
+              >
+                <Icon
+                  type="linkedin"
+                  className="padFAwe"
+                  style={{ fontSize: "35px", color: "#da32ab" }}
+                />
+              </a> */}
+              <span style={{ fontSize: `${1.2}rem`, color: "#db32ab" }}>solcastrodg@gmail.com</span>
+          </Col>
+        </Row>
+      </Fragment>
     )}
   </Spring>
 );
-/*dev branch*/
